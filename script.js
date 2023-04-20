7eda91d711ed97f59220d330cfb8b931ead2ad0c
Wonnie Ki <yewonki@gmail.com>
2023-04-19 19:28:34 -0700

    Remove +1, add if else statements
    Completely unnecessary, thought it was a good idea at the time for some
    reason. -_- Forgot to commit, but stored the answer of Math.floor(Math.
    random()*3) into x, and added if else statement to make each number
    equal "Rock", "Paper", or "Scissors" and return those strings in place
    of the numbers.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1;
+    let x = Math.floor(Math.random() * 3);
+    if (x == 0) {
+        return "Rock";
+    } else if (x == 1) {
+        return "Paper";
+    } else {
+        return "Scissors"
+    }         
 };
 
 console.log (getComputerChoice());
