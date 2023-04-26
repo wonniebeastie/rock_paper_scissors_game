cf74f48564c93d3526005f3a1881db9250d5624c
Wonnie Ki <yewonki@gmail.com>
2023-04-25 20:46:39 -0700

    Add if else of all scenarios and display message

--- script.js
+++ script.js
@@ -23,9 +23,29 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         return "It's a tie!"
+
+        //player wins
+
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! Rock smashes Scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper stifles Rock!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors shred Paper!"
+
+        // computer wins
+
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper stifles Rock!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors shred Paper!"
+    } else {
+        return "You lose! Rock smashes Scissors!"
     }
+
+    //let displayMessage = `You ${}! ${} beats ${}!`
 }
 
 let computerSelection = getComputerChoice();
 
-console.log (playRound("roCK", computerSelection));
+console.log (playRound("SciSsors", computerSelection));
