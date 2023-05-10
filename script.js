d568a346bbc1d6c9958d4ed96217c2d5baf88365
Wonnie Ki <yewonki@gmail.com>
2023-05-09 18:22:00 -0700

    Change to prefixed increment and add to if else

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ function getComputerChoice() {
     }         
 }
 
+
 //#4 & 5 
+
 // let a = "Rock"
 
 // let b = "scissors"
@@ -48,71 +50,82 @@ function playRound(playerSelection, computerSelection) {
 
 
 //#6 
-console.log (game());
 
 function game() {
     let playerInput = prompt("Choose your fighter! Rock, Paper, or Scissors?");
     let playerChoice = playerInput.toLowerCase();
     let computerChoice = getComputerChoice();
 
-    let playerScore = "Player Score: " + 0;
-    let computerScore = "Computer Score: " + 0;
+    let playerScore = 0;
+    let computerScore = 0;
 
-    console.log (playerScore);
-    console.log(computerScore);
+    let gameResult;
 
     console.log (playerChoice);
     console.log (computerChoice);
-
 
     if (playerChoice === "rock" ||
         playerChoice === "paper" ||
         playerChoice === "scissors"
       ) {
-        let gameResult = playRound(playerChoice, computerChoice);
-        return gameResult;
+        gameResult = playRound(playerChoice, computerChoice);
+        console.log (gameResult);
       }
 
 
-    if (gameResult === "It's a tie! Try again.") {
-       
-    } 
-
     if (gameResult.includes("You win!")) {
-
+        console.log ("Your score: " + ++playerScore);
+        console.log ("Computer score: " + computerScore);
+    } else if (gameResult.includes("You lose!")) {
+        console.log ("Your score: " + playerScore);
+        console.log ("Computer score: " + ++computerScore);
+    } else {
+        console.log ("Your score: " + playerScore);
+        console.log ("Computer score: " + computerScore);
     }
-
 }
 
-// run function called game() {
 
-//     create variable called playerInput that stores the answer to the prompt 
-//     "Choose your figher! Rock, Paper, or Scissors?";
+console.log (game());
 
-//     create variable called playerChoice, that stores whatever got stored inside playerInput,
-//     but changed all to lowercase;
+// Declare a function called game() {
 
-//     create variable called computerChoice, which will store the result of the function
-//     getComputerChoice();
+// Create variable called playerInput that stores the answer to the prompt 
+// "Choose your figher! Rock, Paper, or Scissors?";
 
-//     if what was stored inside playerChoice is identical to "rock", "paper", or "scissors",
-//     store the result of the function playRound() - with the parameters of the value for
-//     whatever is stored inside playerChoice, and whatever the result of getComputerChoice()
-//     was - inside ANOTHER variable called gameResult;
+// Create variable called playerChoice, that stores whatever got stored inside playerInput,
+// but changed all to lowercase;
 
-//     return the string stored inside gameResult;
+// Create variable called computerChoice, which will store the result of the function
+// getComputerChoice();
 
-//     if what is returned is "It's a tie!" just run the function playRound(playerChoice,
-//     computerChoice) again;
+// Create variable called playerScore storing "Player Score: " + 0 inside;
 
-//     if what is returned has "You win!" in it, then give 1 point to player;
+// Create variable called computerScore storing "Computer Score: " + 0 inside;
 
-//     if what is returned has "You lose!" in it, then give 1 point to computer; 
+// Display the playerScore on the console;
 
-//     Keep running the function game() until 5 rounds have been won or lost;
+// Display the computerScore on the console; 
 
-//     Announce final winner of the game. 
+// Display the result for playerChoice on the console;
 
+// Display the result for computerChoice on the console;
 
+// if what was stored inside playerChoice is identical to "rock", "paper", or "scissors",
+// store the result of the function playRound() - with the parameters of the value for
+// whatever is stored inside playerChoice, and whatever the result of getComputerChoice()
+// was - inside ANOTHER variable called gameResult;
 
+// return the string stored inside gameResult;
+
+// if what is returned is "It's a tie!" just run the function playRound(playerChoice,
+// computerChoice) again;
+
+// if what is returned has "You win!" in it, then give 1 point to player;
+
+// if what is returned has "You lose!" in it, then give 1 point to computer; 
+
+// Keep running the function game() until 5 rounds have been won or lost;
+
+// Announce final winner of the game. 
 
